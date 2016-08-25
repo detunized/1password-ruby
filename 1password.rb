8a327478d04959857c0585b224317839a42dac04
Dmitry Yakimenko <detunized@gmail.com>
2016-08-25 10:13:39 +0200

    Make actual requests to mitmproxy

--- 1password.rb
+++ 1password.rb
@@ -6,19 +6,38 @@ require "openssl"
 require "base64"
 require "json"
 require "yaml"
+require "uri"
 
 HOST = "https://my.1password.com/api/v1"
 
 SIRP_N = OpenSSL::BN.new "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C934063199FFFFFFFFFFFFFFFF", 16
 SIRP_g = OpenSSL::BN.new 5
 
+# HTTP with proxy
+# mitmproxy must be set up on localhost:8080 with server replay
+# $ mitmproxy -S login.flow --kill --no-pop -s replace.py
 class Http
+    include HTTParty
+
+    def initialize
+        @options = {
+            http_proxyaddr: "localhost",
+            http_proxyport: 8080,
+            verify: false
+        }
+    end
+
     def get url
-        puts "GET #{url}"
+        self.class.get url, @options
     end
 
     def post url, args
-        puts "POST #{url} #{args}"
+        self.class.post url, @options.merge({
+            body: args.to_json,
+            headers: {
+                "Content-Type" => "application/json"
+            }
+        })
     end
 end
 
@@ -58,6 +77,16 @@ module Util
 
     def self.str_to_hex str
         str.unpack("H*")[0]
+    end
+
+    URL_ESCAPE_REGEXP = Regexp.new "[^#{URI::PATTERN::UNRESERVED}]"
+
+    def self.url_escape url
+        URI.escape url, URL_ESCAPE_REGEXP
+    end
+
+    def self.url_escape_join components
+        components.map { |i| url_escape i }.join "/"
     end
 end
 
@@ -147,7 +176,7 @@ class Srp
             "userA" => Util.bn_to_hex(@shared_a)
         }
 
-        response = @http.post "auth", args
+        response = @http.post ["auth"], args
 
         # ==== baked response (for now)
         response = {
@@ -204,6 +233,9 @@ class Srp
 end
 
 class OnePass
+    CONTAINER_TYPE = "b5+jwk+json"
+    ENCRYPTION_MODE = "A256GCM"
+
     def initialize http = nil
         @http = http || Http.new
         @host = "my.1password.com"
@@ -221,7 +253,7 @@ class OnePass
     end
 
     def get_user_info email, uuid
-        get "#{email}/#{uuid}"
+        get ["auth", email, uuid, "-"]
 
         # ==== baked response (for now)
         {
@@ -242,7 +274,7 @@ class OnePass
     def verify_key
         payload = JSON.dump({"sessionID" => @session.id})
         encrypted_payload = encrypt_payload payload, "\0" * 12 # TODO: Generate random
-        post "auth/verify", encrypted_payload
+        post ["auth", "verify"], encrypted_payload
     end
 
     def encrypt_payload plaintext, iv
@@ -251,17 +283,17 @@ class OnePass
         iv_base64 = Util.str_to_base64 iv
 
         {
-            "cty" => "b5+jwk+json",
+            "cty" => CONTAINER_TYPE,
             "data" => ciphertext_base64,
-            "enc" => "A256GCM",
+            "enc" => ENCRYPTION_MODE,
             "iv" => iv_base64,
             "kid" => @session.id
         }
     end
 
     def decrypt_payload payload
-        raise "Unsupported container type '#{cty}'" if (cty = payload["cty"]) != "b5+jwk+json"
-        raise "Unsupported encryption '#{enc}'" if (enc = payload["enc"]) != "A256GCM"
+        raise "Unsupported container type '#{payload["cty"]}'" if payload["cty"] != CONTAINER_TYPE
+        raise "Unsupported encryption '#{payload["enc"]}'" if payload["enc"] != ENCRYPTION_MODE
         raise "Session ID does not match" if payload["kid"] != @session.id
 
         ciphertext = Util.base64_to_str payload["data"]
@@ -299,11 +331,13 @@ class OnePass
     # Http interface
     #
 
-    def get url
+    def get url_components
+        url = Util.url_escape_join url_components
         @http.get "https://#{@host}/api/v1/#{url}"
     end
 
-    def post url, args
+    def post url_components, args
+        url = Util.url_escape_join url_components
         @http.post "https://#{@host}/api/v1/#{url}", args
     end
 end
